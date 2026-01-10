ca709fd5efaccc4fc332708249112ec74dc2e97b
rohangawade-2604 <rohangawade2604@gmail.com>
2026-01-11 00:07:48 +0530

    code

--- Aadrila_project/src/Components/Industries.jsx
+++ Aadrila_project/src/Components/Industries.jsx
@@ -1,24 +1,43 @@
 import React from 'react'
-import circle from "../assets/Images/circle1.png"
+// import circle from "../assets/Images/circle1.png"
 import "../CSS_Components/Industries.css"
+import dots from "../assets/Images/dot.png"
+import dots1 from "../assets/Images/dots.png"
 
 const Industries = () => {
   return (
     <>
-      <section class="section-padding-block ">
+      <section class="section-padding-block">
+        <div className="industries">
+
+       
         <div class="container">
-          <div class=" industries-section">
 
-            <div className="circle">
-              <img src={circle} alt="circle" />
-            </div>
+          <div className="section-cards">
 
-             <div className="col-md-5 AI-content  ">
+            <div class="industries-section">
+
+              <div className=" AI-content  ">
                 <p className="text-orange">AI-driven innovation for growth.</p>
                 <h2 className="">Industries We Empower</h2>
               </div>
+
+              <div className="dots">
+                  <img src={dots} alt="" />
+              </div>
+
+              <div className="dots1">
+                <img src={dots1} alt="" />
+              </div>
+
+            </div>
+
           </div>
+
         </div>
+         </div>
+
+      
       </section>
 
     </>
